762fdc76a29a5119c57ded9b41f58e652b6f9222
Ancoder2009 <68029104+Ancoder2009@users.noreply.github.com>
2021-04-13 09:44:27 -0700

    Update scripts.js

--- tests/js/scripts.js
+++ tests/js/scripts.js
@@ -1,1 +1,3 @@
+console.log("Main js loaded in.")
+console.log("Don't mess with the console if you do not know what you are doing. Don't PASTE in code that someone gave you.")
 $(".home").href = "/"
